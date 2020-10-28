1a30f393cc50861ece0517e77d6d71c9036d704d
Delfino-app <60131914+Delfino-app@users.noreply.github.com>
2020-10-28 09:32:40 +0000

    Lasts Updates

---
+++ FrontEnd/public/js/utils/validateImage.js
@@ -0,0 +1,48 @@
+
+export default{
+
+	validateImg(file){
+
+		const img = file;
+
+		const img_name = img.name;
+		const img_extension = img_name.split('.').pop().toLowerCase();
+		const extension_valida = ['png','jpg','jpeg'];
+
+		const img_size = img.size;
+
+		//Retorno 
+		const retorno = {
+
+			status:'',
+			info:'',
+			message:''
+		}
+
+		//Validar Exetensão
+		if(jQuery.inArray(img_extension,extension_valida) == -1){
+
+			retorno.status = 400;
+			retorno.info = 'Ficheiro de imagem inválido.';
+			retorno.message = `São apenas permitidos ficheiros do tipo ${extension_valida[0]}, ${extension_valida[1]} e ${extension_valida[2]}`;
+		}
+		else if(img_size > 2000000){
+
+			retorno.status = 400;
+			retorno.info = 'Ficheiro de imagem muito grande.';
+			retorno.message = `O tamanho da imagem não pode ser superior a 2MB`;
+		}
+		else{
+
+			retorno.status = 200;
+			retorno.info = 'Imagem validada com sucesso';
+			retorno.message = `Já podes fazer Upload da imagem`;
+		}
+
+		return retorno;
+	}
+}
+
+
+
+
